7fb46e095b5fbfd0422e27a5030ba41f3f2db942
Henrique Mauler <henriquemauler@gmail.com>
2024-11-08 20:58:32 -0300

    limpeza

--- lib/Cassandra/main.py
+++ lib/Cassandra/main.py
@@ -1,143 +1,31 @@
 # from lib.Cassandra.lib.test_flow import SchemaContext
-from lib.test_flow import *
+
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import time
+from lib.Iexecutable import Container
+from lib.context import TestContext
+from lib.output import NoConsole, RichConsole
+from lib.schema_manager import DefaultSchemaManager
+from lib.serial_type import ArrayValidator, DateBaseFormat, GenericObjectValidator, IntegerValidator, NumberValidator, StringValidator
+from lib.serializer import YamlSerializer
+from lib.snapshot_manager import SnapshotManager
+from lib.utils import hook_status_in_context
+
+
 rich_console = RichConsole()
 no_console = NoConsole()
 test_instance = Container(name = 'root', output = no_console)
 
 happy_path = test_instance.create_container('scenario: o caminho feliz')
 
-generic_object_validator = BaseValidatorManager()
-# serializer = YamlSerializer(folder = 'mock')
-
 schema_object_validator = YamlSerializer(folder = 'dist', name = 'data.yaml')
-default_schema_manager = DefaultSchemaManager(validator = generic_object_validator, serializer = schema_object_validator)
+default_schema_manager = DefaultSchemaManager(serializer = schema_object_validator)
 
 
 
 ctx = TestContext(no_console)
 hook_status_in_context(ctx)
-
-
-from datetime import datetime
-
-def is_valid_date(date_string, date_format="%Y-%m-%d"):
-    """
-    Verifica se uma string é uma data válida.
-
-    :param date_string: A string a ser validada.
-    :param date_format: O formato da data (padrão: "%Y-%m-%d").
-    :return: True se a string for uma data válida, False caso contrário.
-    """
-    try:
-        datetime.strptime(date_string, date_format)
-        return True
-    except ValueError:
-        return False
-
-class DateBaseFormat(SerialType):
-
-    def acceptable_field_type(self):
-        return str,
-
-    def validate(self, ctx: SchemaContext):
-        if isinstance(ctx.field, str) and is_valid_date(ctx.field):
-            ctx.approve()
-        else:
-            ctx.reject("tipo inválido")
-            
-    def schema_format(self):
-        return {'type' : "date"}
-
-class ArrayValidator(SerialType):
-
-    def acceptable_field_type(self):
-        return list,
-
-    def validate(self, ctx: SchemaContext):
-        if isinstance(ctx.field, list):
-            ctx.approve()
-        else:
-            ctx.reject()
-
-    def schema_format(self):
-        return {'type' : 'list'}
-
-
-class StringValidator(SerialType):
-
-    def acceptable_field_type(self):
-        return str,
-
-    def validate(self, ctx: SchemaContext):
-        if isinstance(ctx.field, str):
-            ctx.approve()
-        else:
-            ctx.reject()
-
-    def schema_format(self):
-        return {'type' : 'text'}
-
-
-class NumberValidator(SerialType):
-
-    def acceptable_field_type(self):
-        return float,
-
-    def validate(self, ctx: SchemaContext):
-        if isinstance(ctx.field, float):
-            ctx.approve()
-        else:
-            ctx.reject()
-
-    def schema_format(self):
-        return {'type' : 'number'}
-
-
-class GenericObjectValidator(SerialType):
-
-    def acceptable_field_type(self):
-        return dict,
-
-    def validate(self, ctx: SchemaContext):
-        if isinstance(ctx.field, dict):
-            ctx.approve()
-        else:
-            ctx.reject()
-
-    def schema_format(self):
-        return {'type' : 'object'}
-
-
-
-class IntegerValidator(SerialType):
-
-    def acceptable_field_type(self):
-        return int,
-
-    def validate(self, ctx: SchemaContext):
-        if isinstance(ctx.field, int):
-            ctx.approve()
-        else:
-            ctx.reject()
-
-    def schema_format(self):
-        return {'type' : 'integer'}
-
-class BooleanValidator(SerialType):
-
-    def acceptable_field_type(self):
-        return bool,
-
-    def validate(self, ctx: SchemaContext):
-        if isinstance(ctx.field, bool):
-            ctx.approve()
-        else:
-            ctx.reject()
-
-    def schema_format(self):
-        return {'type' : 'boolean'}
-
 
 
 
@@ -208,10 +96,10 @@ def assertions(ctx: TestContext):
     ctx.approve()
 
 
-test_instance.before_each(load_snapshot, isolated = False)
+test_instance.before(load_snapshot, isolated = False)
 # test_instance.add_step("tirar fotos", take_photos, isolated = False)
 test_instance.add_step("assertions", assertions, isolated = False) # assertions
-# test_instance.after_each(save_snapshot, isolated = False)
+# test_instance.after(save_snapshot, isolated = False)
 
 
 test_instance.execute(context=ctx, event_loop=asyncio.new_event_loop(), thread_pool_executor=ThreadPoolExecutor(max_workers=10, thread_name_prefix="pipe"))
