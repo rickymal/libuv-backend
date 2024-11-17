8e945460bf2f3fac13b5221c134569036e7e849d
Henrique Mauler <henriquemauler@gmail.com>
2024-11-17 18:51:05 -0300

    adicionado sequenciador

--- apps/Luma/main_v3.py
+++ apps/Luma/main_v3.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass, field
 from collections import defaultdict
 from enum import Enum, auto
 import re
+from typing import Union
+
 
 def regex_finder(text: str, pattern: str) -> list[tuple[int, int, str]]:
     matches = []
@@ -10,56 +12,75 @@ def regex_finder(text: str, pattern: str) -> list[tuple[int, int, str]]:
         matches.append((match.start(), match.end(), match.group()))
     return matches
 
-open_bracket = dict(scan = lambda text: regex_finder(text, pattern="<"), right = [], sequence = False, optional = False)
-slash = dict(scan = lambda text: regex_finder(text, pattern="/"), right = [], sequence = False, optional = False)
-close_bracket = dict(scan = lambda text: regex_finder(text, pattern=">"), right = [], sequence = False, optional = False)
+
+@dataclass
+class TK:
+    scan: callable
+
+@dataclass
+class TKP:
+    name: str
+    tk: TK
+    sequence: bool
+    optional: bool
+
+    def __repr__(self):
+        return f"TKP: {self.name}"
+
+open_bracket = TK(scan = lambda text: regex_finder(text, pattern="<"))
+slash = TK(scan = lambda text: regex_finder(text, pattern="/"))
+close_bracket = TK(scan = lambda text: regex_finder(text, pattern=">"))
+
+@dataclass
+class Chain:
+    def __init__(self, value = None):
+        self.value = value
+        self.next: list[Chain | TKP] = []
+        self.pos = 0
 
 
-def and_selector(*tks, visitor: callable):
-    d_tks = tuple(tks)
-    r_tks = d_tks[::-1]
-    dd = dict()
-
-    for tk1, tk2 in zip(d_tks[:-1], d_tks[1:]):
-        tk1['right'].append(tk2)
-
-        if tk1['sequence']:
-            tk1['right'].append(tk1)
+    def set_next(self, tk: Union['Chain', TK]):
+        ch = Chain()
+        ch.value = tk
+        self.next.append(ch)
 
 
-    for tk1, tk2 in zip(r_tks[:-1], r_tks[1:]):
-        if tk1['optional'] and len(tk1['right']) > 0:
-            tk2['right'](*tk1['right'])
-        pass
+def and_selector(*tks: TKP | Chain) -> Chain:
+    lof: list[Chain] = [Chain(val) for val in tks]
 
-    for idx, tk in enumerate(d_tks):
-        yield idx, tk
+    direct = tuple(lof)
+    reverse = direct[::-1]
 
-def one_selector(tk:dict[int, dict]):
-    yield {
-        'pos': 0,
-        'tk': tk,
-        'right': [],
-        'sequence' : False,
-        'optional': False
-    }
+    for ch1, ch2 in zip(direct[:-1], direct[1:]):
+        ch1.next.append(ch2)
 
-    raise StopIteration("")
+        if ch1.value.sequence:
+            ch1.next.append(ch1)
 
-def zero_or_one_selector(tk: dict[int, dict]):
-    yield {
-        'pos': 0,
-        'tk': tk,
-        'right': [],
-        'optional' : True,
-        'sequence' : False,
-    }
-
-    raise StopIteration("")
+    for ch1, ch2 in zip(reverse[:-1], reverse[1:]):
+        if ch1.value.optional:
+            ch2.next.append(*ch1.next)
 
 
+    for idx, val in enumerate(lof):
+        val.pos = idx
 
-for val in and_selector(open_bracket, slash, close_bracket, visitor=None):
-    print(val)
-    pass
+    return lof[0]
 
+
+def or_selector(*tks: TKP, input_ch: Chain, output_ch: Chain):
+    lof: list[Chain] = [Chain(val) for val in tks]
+    for ch in lof:
+        input_ch.next.append(ch)
+        ch.next.append(output_ch)
+
+
+v = and_selector(
+TKP(name = 'OPEN_BRACKET', tk = open_bracket, sequence=False, optional=False),
+    or_selector(TKP(name = 'SLASH', tk = slash, sequence=False, optional=False),),
+    TKP(name = 'CLOSE_BRACKET', tk = close_bracket, sequence=False, optional=False)
+)
+from IPython import embed
+embed()
+
+primeiro = v.next[0]
